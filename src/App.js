325ac179be37983b58b16104b7e37feb40394a6e
rosier101 <rosier101@gmail.com>
2021-02-07 14:02:32 -0500

    added buttons page for profile choice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//Importating Components
+import Buttons from "./components/Buttons";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+
+      <Buttons/>
       
     </div>
   );
