b954c0082e8902523e3eecb3338b6ce62b0c356a
rosier101 <rosier101@gmail.com>
2021-02-07 14:07:15 -0500

    adding untracked files

---
+++ src/components/Buttons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Buttons extends React.Component {
+    render () {
+        return (
+
+        <div>
+            <h1>Choose A Profile</h1>
+        </div>
+            )
+
+        
+    }
+}
+
+export default Buttons;
