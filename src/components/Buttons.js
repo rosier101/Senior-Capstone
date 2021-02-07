0349ac72fd18f9adf384dc26905d52c45154c800
rosier101 <rosier101@gmail.com>
2021-02-07 14:50:52 -0500

    first button test `

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Buttons.css';
 
 class Buttons extends React.Component {
     render () {
@@ -6,7 +7,27 @@ class Buttons extends React.Component {
 
         <div>
             <h1>Choose A Profile</h1>
+            
+            {//Different Types of Buttons for each profile
+            }
+            <div class="container">
+    <div class="center">
+      <button class="btn">
+        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span>Kim Kardashian</span>
+      </button>
+    </div>
+  </div>
+            
         </div>
+
+
+
+
+        
             )
 
         
