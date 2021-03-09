406bfcf0fe5c273409aa627f7f85522f434f9c2b
rosier101 <rosier101@gmail.com>
2021-03-08 20:01:45 -0500

    added header and navbar items

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 //Importating Components
 import Buttons from "./components/Buttons";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+
+<Header/>
       <h1>Hello</h1>
 
       <Buttons/>
