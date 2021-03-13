4aea8d54a7cd174aa7951ca35cc81304cf3ed108
rosier101 <rosier101@gmail.com>
2021-03-12 22:32:43 -0500

    added profiles to homepage

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -10,22 +10,48 @@ class Buttons extends React.Component {
             
             {//Different Types of Buttons for each profile
             }
-            <div class="container">
-    <div class="center">
-      <button class="btn">
-        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
-          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
-          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
-        </svg>
-        <span>Kim Kardashian</span>
-      </button>
-    </div>
+          <div class="container">
+            <div class="shop-items">
+                <div class="shop-item">
+                  <span class="shop-item-title">Kim Kardashian</span>
+                  <img class="shop-item-image" src = "/images/KimK.jpg" alt = "smiley"/>
+                    <div class="shop-item-details">
+                    <div class="center">
+                      <button class="btn">
+                        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                        </svg>
+                        <span>Kim Kardashian</span>
+                      </button>
+                    </div>
+                    </div>
 
-    <div class = "btn2">
-        <a href="#" class="btn-flip" data-back="Kardashian" data-front="Kim"></a>
-        </div>
+                </div>
+              </div>
+
+
+                      
+                        <div class="shop-items">
+                          <div class="shop-item">
+                          <span class="shop-item-title">The Rock</span>
+                          <img class="shop-item-image" src = "/images/TheRock.jpg" alt = "smiley"/>
+                          <div class="shop-item-details">
+                          <div class="center">
+                          <div class = "btn2">
+                          <a href="#" class="btn-flip" data-back="Rock" data-front="The"></a>
+                          </div>
+                          </div>
+                          </div>
+                          </div>
+                          </div>
+                          
     
-  </div>
+          
+          
+          
+          
+          </div>
             
         </div>
 
